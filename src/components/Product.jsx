7c78f78902ed47194df387463c93448055e0acf2
Arief R Syauqie <arief0303@gmail.com>
2024-04-18 05:44:44 +0700

    product selection

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -26,7 +26,7 @@ const Solution = () => {
             description: '',
             imgSrc: '/svg/bullet-7.svg',
             links: [
-                { url: 'https://example.com/link3', text: 'Link 3' },
+                { url: 'https://holistic-overlay-429385.framer.app/', text: 'Flyer B-School' },
             ],
         },
     ];
@@ -50,15 +50,57 @@ const Solution = () => {
     ];
 
     const listItemsCompany = [
-        // Add the items for 'Company' here
+        {
+            text: 'Universitas',
+            description: 'Telah Digunakan Lebih Dari 1200 Sekolah.',
+            imgSrc: '/svg/bullet-6.svg',
+            links: [
+            ],
+        },
+        {
+            text: 'Flyer Universitas',
+            description: '',
+            imgSrc: '/svg/bullet-7.svg',
+            links: [
+                { url: 'https://example.com/link3', text: 'Link 3' },
+            ],
+        },
     ];
 
     const listItemsKoperasi = [
-        // Add the items for 'Koperasi' here
+        {
+            text: 'Universitas',
+            description: 'Telah Digunakan Lebih Dari 1200 Sekolah.',
+            imgSrc: '/svg/bullet-6.svg',
+            links: [
+            ],
+        },
+        {
+            text: 'Flyer Universitas',
+            description: '',
+            imgSrc: '/svg/bullet-7.svg',
+            links: [
+                { url: 'https://example.com/link3', text: 'Link 3' },
+            ],
+        },
     ];
 
     const listItemsGeneral = [
-        // Add the items for 'General' here
+        {
+            text: 'General',
+            description: 'Beberapa Komunitas Telah Menggunakan Closepay',
+            imgSrc: '/svg/bullet-6.svg',
+            links: [
+            ],
+        },
+        {
+            text: 'Flyer General',
+            description: '',
+            imgSrc: '/svg/bullet-7.svg',
+            links: [
+                { url: 'https://example.com/link3', text: 'Flyer General' },
+            ],
+        },
     ];
 
     const listItemsMap = {
@@ -71,32 +113,35 @@ const Solution = () => {
 
     return (
         <div className='h-full bg-white py-10'>
-            <div className='flex flex-col justify-center items-center w-full py-10'>
-                <h3 className='text-primary text-4xl pb-8' data-aos="zoom-in">Product & Solution</h3>
-                <h1 className="text-4xl font-extrabold pb-5 mx-7 text-black">{`Produk Kolaborasi`.split(' ').map((word, index) => (
-                    <span key={index} data-aos="fade-up" data-aos-delay={`${index * 500}`}>{word} </span>
-                ))}
-                </h1>
-            </div>
-
-            <div className='flex justify-center'>
-                {buttons.map((button, index) => (
-                    <button
-                        key={index}
-                        type="button"
-                        className={`${activeButton === button ? 'textz-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' : 'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'}`}
-                        onClick={() => setActiveButton(button)}
-                    >
-                        {button}
-                    </button>
-                ))}
+            <div className='flex flex-column lg:flex-row'>
+                <div className='flex flex-col lg:flex-row w-full'>
+                    <h3 className='text-primary text-4xl mx-7 pb-8' data-aos="zoom-in">Product & Solution</h3>
+                    <h1 className="text-4xl font-extrabold pb-5 mx-7 text-black">{`Produk Kolaborasi`.split(' ').map((word, index) => (
+                        <span key={index} data-aos="fade-up" data-aos-delay={`${index * 500}`}>{word} </span>
+                    ))}
+                    </h1>
+                    <div className='flex flex-row flex-wrap h-1/2 w-full justify-center px-24 mt-9'>
+                        {buttons.map((button, index) => (
+                            <button
+                                key={index}
+                                type="button"
+                                className={`${activeButton === button ? 'textz-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' : 'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'}`}
+                                onClick={() => setActiveButton(button)}
+                            >
+                                {button}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </div>
 
             <div className='flex flex-col sm:flex-row justify-center'>
                 {(() => {
                     switch (activeButton) {
                         case 'Universitas':
-                            return <img src="/images/universitas.webp" className="w-full h-full" alt="" />;
+                            return (
+                                <img src='/images/universitas.webp' alt='universitas' className='h-[400px] w-[600px] object-cover rounded-xl mx-10' />
+                            )
                         case 'Sekolah':
                             return (
                                 <>
@@ -112,9 +157,10 @@ const Solution = () => {
                                 </>
                             );
                         case 'Company':
-                            return <img src="/images/company.webp" className="w-full h-full" alt="" />;
+                            return <img src='/images/company.webp' alt='company' className='h-[400px] w-[600px] object-cover rounded-xl mx-10' />;
                         case 'Koperasi':
-                            return <img src="/images/koperasi.webp" className="w-full h-full" alt="" />;
+                            return <img src='/images/koperasi.webp' alt='koperasi' className='h-[400px] w-[600px] object-cover rounded-xl' />
+                                ;
                         case 'General':
                             return (
                                 <>
@@ -124,7 +170,7 @@ const Solution = () => {
                                         <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
                                         <div className="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
                                         <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-gray-800">
-                                            <img src="/images/pspmobile.webp" className="w-[272px] h-[572px]" alt="" />
+                                            <img src="/images/general.webp" className="w-[272px] h-[572px]" alt="" />
                                         </div>
                                     </div>
                                 </>
@@ -148,15 +194,15 @@ const Solution = () => {
 
                 <div className='w-fit pb-10 pl-10'>
                     {listItemsMap[activeButton].map((item, index) => (
-                        <div key={index} className='flex items-center py-2' data-aos="zoom-in-right" data-aos-delay={`${index * 300}`}>
+                        <div key={index} className='flex items-center py-4' data-aos="zoom-in-right" data-aos-delay={`${index * 300}`}>
                             <div className='flex-none p-1 rounded-lg bg-white m-2 w-10 h-10'>
                                 <img className="h-8" src={item.imgSrc} alt="digital card" />
                             </div>
-                            <div className='text-black w-80 sm:w-full'>
-                                <p>{item.text}</p>
-                                {item.description && <p>{item.description}</p>}
+                            <div className='w-80 sm:w-full'>
+                                <h2 className='text-neutral-950 text-xl'>{item.text}</h2>
+                                {item.description && <p className=' text-neutral-500'>{item.description}</p>}
                                 {item.links && item.links.map((link, index) => (
-                                    <a key={index} href={link.url} className='btn-class'>{link.text}</a>
+                                    <a href='https://holistic-overlay-429385.framer.app/' className=" text-blue-700">{link.text}</a>
                                 ))}
                             </div>
                         </div>
